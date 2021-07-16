aa934b1dbf0e82b0773fe62448d011f53e4344ca
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-07-16 11:43:16 -0400

    Initial Commit, adding finished solution code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Character from './components/Character'
+import styled, { keyframes } from 'styled-components'
 import './App.css';
+
+const glow = keyframes`
+  from{
+    text-shadow: 4px 3px 0px #fff, 9px 8px 0px #B62321;
+  }
+  to{
+    text-shadow: 2px 3px 0px  #15509F;
+  }
+`;
+
+const TitleText = styled.div`
+  position:relative;
+  z-index: 1;
+  display: inline-block;
+  text-align:center;
+  font-family:'Monoton',sans-serif;
+  font-size: 5rem;
+  font-weight: 400;
+  margin: 2vh 0;
+  margin-bottom: 10px;
+  letter-spacing: 3px;
+
+  ::before{
+    position: absolute;
+    z-index: -1;
+    inset:0;
+    content: 'Star Wars';
+    animation: ${glow} 2s ease infinite alternate;
+    
+  }
+`
+
+const CharactersContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  align-items: center;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [ characterData, setCharacterData ] = useState(null);
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then( response => setCharacterData(response.data))
+  }, [])
+
+  console.log(characterData)
+
+  if (!characterData) return <h1>Loading</h1>
+
   return (
     <div className="App">
+      <TitleText d>Star Wars</TitleText>
       <h1 className="Header">Characters</h1>
+      <CharactersContainer>
+      {
+        characterData.map((character) => (
+          <Character data={character}/>
+        ))
+      }
+      </CharactersContainer>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,65 @@
-// Write your Character component here
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const CharacterCard = styled.div`
+  width: 60%;
+  background-color: rgba(0, 0, 0, 0.25);
+  margin: 1.5vh;
+  padding: 1vh 35px;
+  border-radius: 3px;
+  box-shadow: 1px 1px 15px #2e2e2e;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const PreviewContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  p {
+    font-size: 1.25rem;
+  }
+
+  p:last-child {
+    background-color: gray;
+    padding: 2px 20px;
+    border-radius: 50px;
+  }
+`;
+
+const DetailContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+function Character(props) {
+  const { name, birth_year, gender, height, mass } = props.data;
+
+  const [cardToggled, setCardToggled] = useState(false);
+
+  const toggleCard = () => {
+    cardToggled !== true ? setCardToggled(true) : setCardToggled(false);
+  };
+
+  return (
+    <CharacterCard onClick={() => toggleCard()}>
+      <PreviewContent>
+        <p>{name}</p>
+        <p>{birth_year}</p>
+      </PreviewContent>
+      {cardToggled ? (
+        <DetailContent>
+          <p> Gender: {gender}</p>
+          <p> Height: {height}</p>
+          <p> Mass: {mass}</p>
+        </DetailContent>
+      ) : null}
+    </CharacterCard>
+  );
+}
+
+export default Character;
